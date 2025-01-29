225a47a7706e380b94512f353293a3d043a54ec9
Marlon Valbuena <marlon_l_g@outlook.com>
2025-01-28 19:27:30 -0500

    Ajuste para el funcionamiento del cloud-gateway

--- ms-books-payments/src/main/java/com/relato/ms_books_payments/service/ComprasService.java
+++ ms-books-payments/src/main/java/com/relato/ms_books_payments/service/ComprasService.java
@@ -30,24 +30,28 @@ public class ComprasService {
     }
     
     public boolean verificarStock(Long libroId, Long cantidad) {
-        ServiceInstance instance = discoveryClient.getInstances("MS-BOOKS-CATALOGUE").stream().findFirst().orElse(null);
+        ServiceInstance instance = discoveryClient.getInstances("CLOUD-GATEWAY").stream().findFirst().orElse(null);
         if (instance == null) {
-            throw new RuntimeException("No instances of MS-BOOKS-CATALOGUE found");
+            throw new RuntimeException("No instances of CLOUD-GATEWAY found");
         }
         String url = instance.getUri().toString() + "/libros/" + libroId + "/cantidad";
         stockDisponible = restTemplate.getForObject(url, Long.class);
+        System.out.println("Stock disponible: " + stockDisponible);
         return cantidad <= stockDisponible;
     }
     
     public Compras registrarCompra(Compras compra) {
-        ServiceInstance instance = discoveryClient.getInstances("MS-BOOKS-CATALOGUE").stream().findFirst().orElse(null);
+        ServiceInstance instance = discoveryClient.getInstances("CLOUD-GATEWAY").stream().findFirst().orElse(null);
         if (instance == null) {
-            throw new RuntimeException("No instances of MS-BOOKS-CATALOGUE found");
+            throw new RuntimeException("No instances of CLOUD-GATEWAY found");
         }
         Long nuevaCantidad = stockDisponible - compra.getCantidad();
         String url = instance.getUri().toString() + "/libros/" + compra.getLibroId() + "/cantidad?cantidad=" + nuevaCantidad;
         restTemplate.put(url, null);
         return compraRepository.save(compra);
     }
+
+    
+
 
 }
